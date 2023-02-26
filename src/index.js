e77a16208a03252c58b836da7b75105205550207
Goody <101070150+Goody4444@users.noreply.github.com>
2023-02-26 19:45:48 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import Picker, { SKIN_TONE_MEDIUM_DARK } from 'emoji-picker-react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// import { SkinTonePickerLocation } from 'emoji-picker-react';
+
+const App = () => {
+    const [chosenEmoji, setChosenEmoji] = useState(null);
+
+    const onEmojiClick = (event, emojiObject) => {
+        setChosenEmoji(emojiObject);
+    }
+
+    return (
+      <>
+      <h1 style={{textAlign: 'center'}}>Demo emoji picker</h1>
+        <div style={{textAlign: 'center',marginLeft:'810px'}}>
+            <Picker  onEmojiClick={onEmojiClick} skinTone={SKIN_TONE_MEDIUM_DARK}/>
+            { chosenEmoji && <EmojiData chosenEmoji={chosenEmoji}/>}
+        </div>
+        </>
+    );
+};
+
+const EmojiData = ({chosenEmoji}) => (
+  <div style={{textAlign: 'center',marginRight: '810px'}}>
+    <br></br>
+    <br></br>
+    <hr></hr>
+    <strong>Names:</strong> {chosenEmoji.names.join(', ')}<br/>
+    <strong>Symbol:</strong> {chosenEmoji.emoji}<br/>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(<App />, document.getElementById('root'));
